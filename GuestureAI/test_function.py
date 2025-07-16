41207396cb68505deb09f69f612b5853118f831a
LouisZhu040 <zym1670308239@outlook.com>
2025-07-16 11:58:59 +0800

    Add files via upload

---
+++ GuestureAI/test_function.py
@@ -0,0 +1,45 @@
+import torch
+from model import MyCNNModel
+from data_preprocess import process_data
+from torch.utils.data import TensorDataset, DataLoader
+
+
+
+
+def debug_check(Y_val, outputs):
+    import numpy as np
+    _, predicted = torch.max(outputs, 1)
+    print(" Val Label Distribution:", np.bincount(Y_val))
+    print(" Model Prediction Distribution:", torch.bincount(predicted))
+
+
+def test(model, val_loader, criterio):
+    # Start testing the model
+    model.eval()  # set the model to evaluation mode
+    correct = 0     # Initialize the number of correct predictions
+    total = 0       # Initialize the total number of samples
+
+    # Disable gradient calculation for validation to save memory and computation
+    # This is useful because we don't need to compute gradients during validation
+    with torch.no_grad():  
+        # Loop through each batch of validation data
+        for images, labels in val_loader:
+            outputs = model(images)             # Forward pass: compute the model's predictions
+
+            debug_check(labels.numpy(), outputs)
+
+
+            # outputs.data contains the raw output from the model
+            # torch.max() returns the maximum value and its index along the specified dimension
+            # Because the output is a 2D tensor, we use dim=1 to find the maximum value along the class dimension
+            # we don't need the maximum value as we only want to know which class the model chooses, so we use _ to ignore it
+            _, predicted = torch.max(outputs.data, 1)  # _, predicted is named "unpacking", which means we only need the second value (the predicted class)
+
+            total += labels.size(0)                          # The total number of samples
+            correct += (predicted == labels).sum().item()    # Count the number of correct predictions
+
+    # Print the accuracy of the model on the validation set
+    accuracy = 100 * correct / total
+    print(f'Accuracy on validation set: {accuracy:.2f}%')
+    return accuracy
+
